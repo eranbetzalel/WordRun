268e914766729b47f5f1525b209287a3f6ede82e
eranbetzalel <eran.betzalel@gmail.com>
2013-12-03 17:33:16 +0200

    UI fixes.

--- src/public/js/chat.js
+++ src/public/js/chat.js
@@ -1,65 +1,7 @@
 ﻿define(
-  ['config', 'jquery', 'html5shiv', 'ractive', 'moment', 'socket.io'],
-  function (config, $, html5Shiv, ractive, moment, io) {
+  ['config', 'jquery', 'html5shiv', 'ractive', 'moment', 'socket.io', 'stub'],
+  function (config, $, html5Shiv, ractive, moment, io, stubData) {
     'use strict';
-
-    var stubUser = {};
-
-    var stubRoom = { usersCount: 100 };
-
-    var user1Conversation = {
-      user: stubUser,
-      activeConversation: true,
-      messages: [
-          { time: '11:22:00', userName: 'User 1', text: 'Testing messages...1' },
-          { time: '11:22:00', userName: 'User 1', text: 'Testing messages...2' },
-          { time: '11:22:00', userName: 'User 1', text: 'Testing messages...3' }
-      ]
-    };
-
-    var stubData = {
-      user: {
-        isLoggedIn: false,
-        profile: {
-          nick: '',
-          gender: '',
-          age: ''
-        }
-      },
-      users: {
-        'User 1': stubUser,
-        'User 2': {},
-        'User 3': {},
-        'User 4': {},
-        'User 5': {}
-      },
-      rooms: {
-        'Lobby': stubRoom,
-        'Room1': { usersCount: 110 },
-        'Room2': { usersCount: 120 }
-      },
-      selectedConversation: user1Conversation,
-      roomConversation: {
-        room: stubRoom,
-        roomName: 'Lobby',
-        messages: [
-            { time: '11:22:00', userName: 'User1', text: 'Testing messages...1' },
-            { time: '11:22:00', userName: 'User2', text: 'Testing messages...2' },
-            { time: '11:22:00', userName: 'User3', text: 'Testing messages...3' }
-        ]
-      },
-      userConversations: {
-        'User 1': user1Conversation,
-        'User 2': {
-          user: stubUser,
-          messages: [
-              { time: '11:22:00', userName: 'User 2', text: 'Testing messages...1' },
-              { time: '11:22:00', userName: 'User 2', text: 'Testing messages...2' },
-              { time: '11:22:00', userName: 'User 2', text: 'Testing messages...3' }
-          ]
-        }
-      }
-    };
 
     var ChatApp = ractive.extend({
       el: 'chatApp',
@@ -134,6 +76,9 @@
           sendMessage: function (event) {
             event.original.preventDefault();
 
+            if(textToSend.value === '')
+              return;
+
             var messages = this.data.selectedConversation.messages;
 
             if (messages.length > config.maxTextMessages)
@@ -141,8 +86,8 @@
 
             messages.push(
               {
-                time: new Date(),
-                userName: this.data.user.name,
+                time: moment().format('HH:mm:ss'),
+                userName: this.data.user.profile.name,
                 text: textToSend.value
               });
 
---
+++ src/public/js/stub.js
@@ -0,0 +1,64 @@
+﻿define(
+  function () {
+    'use strict';
+    
+    var stubUser = {};
+
+    var stubRoom = { usersCount: 100 };
+
+    var user1Conversation = {
+      user: stubUser,
+      activeConversation: true,
+      messages: [
+          { time: '11:22:00', userName: 'User 1', text: 'Testing messages...1' },
+          { time: '11:22:00', userName: 'User 1', text: 'Testing messages...2' },
+          { time: '11:22:00', userName: 'User 1', text: 'Testing messages...3' }
+      ]
+    };
+
+    var stubData = {
+      user: {
+        isLoggedIn: false,
+        profile: {
+          name: 'Test',
+          gender: '',
+          age: ''
+        }
+      },
+      users: {
+        'User 1': stubUser,
+        'User 2': {},
+        'User 3': {},
+        'User 4': {},
+        'User 5': {}
+      },
+      rooms: {
+        'Lobby': stubRoom,
+        'Room1': { usersCount: 110 },
+        'Room2': { usersCount: 120 }
+      },
+      selectedConversation: user1Conversation,
+      roomConversation: {
+        room: stubRoom,
+        roomName: 'Lobby',
+        messages: [
+            { time: '11:22:00', userName: 'User1', text: 'Testing messages...1' },
+            { time: '11:22:00', userName: 'User2', text: 'Testing messages...2' },
+            { time: '11:22:00', userName: 'User3', text: 'Testing messages...3' }
+        ]
+      },
+      userConversations: {
+        'User 1': user1Conversation,
+        'User 2': {
+          user: stubUser,
+          messages: [
+              { time: '11:22:00', userName: 'User 2', text: 'Testing messages...1' },
+              { time: '11:22:00', userName: 'User 2', text: 'Testing messages...2' },
+              { time: '11:22:00', userName: 'User 2', text: 'Testing messages...3' }
+          ]
+        }
+      }
+    };
+
+    return stubData;
+  });
